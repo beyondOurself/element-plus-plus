1afeca852cd3b20890e321618e14edc49ac6aaae
canlong.shen <562172151@qq.com>
2023-07-11 10:35:08 +0800

    feat: 新增 on-change 事件;

--- src/components/bsgoal-base-time-range/demo.vue
+++ src/components/bsgoal-base-time-range/demo.vue
@@ -2,7 +2,7 @@
  * @Author: canlong.shen
  * @Date: 2023-05-24 14:10:03
  * @LastEditors: canlong.shen
- * @LastEditTime: 2023-05-24 16:40:42
+ * @LastEditTime: 2023-07-11 10:34:27
  * @FilePath: \common\src\components\bsgoal-base-time-range\demo.vue
  * @Description: 时间选择 演示 
  * 
@@ -22,19 +22,33 @@ const props = defineProps({})
 
 const timeRange = ref(['11:00', '12:12'])
 
+const timeList = ref([
+  {
+    startTime: '00:00',
+    endTime: '01:00'
+  }
+])
+
 const startTime = ref('12:11')
 const endTime = ref('13:55')
+
+const changeTime = (start = '' , end = '') => {
+    console.log('start',start);
+    console.log('end',end);
+}
 </script>
 <template>
   <div class="bsgoal-base-time-range-demo">
     <div>{{ timeRange }}</div>
     <div>{{ startTime }}</div>
     <div>{{ endTime }}</div>
+    <div>{{ timeList[0]['startTime'] }}</div>
+    <div>{{ timeList[0]['endTime'] }}</div>
     <BsgoalBaseTimeRange
-      v-model:startTime="startTime"
-      v-model:endTime="endTime"
-      v-model="timeRange"
+      :body-style="{ width: '100%' }"
+      :modelValue="[timeList[0]['startTime'], timeList[0]['endTime']]"
       class="base_time_range_demo"
+      @on-change="changeTime"
     />
   </div>
 </template>
