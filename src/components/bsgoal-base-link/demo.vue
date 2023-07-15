a1bb463ca4aa00f8d04571e07f4610021ca48949
canlong.shen <562172151@qq.com>
2023-07-15 19:38:24 +0800

    feat: 链接 新增类型;

--- src/components/bsgoal-base-link/demo.vue
+++ src/components/bsgoal-base-link/demo.vue
@@ -2,8 +2,8 @@
  * @Author: canlong.shen
  * @Date: 2023-05-10 13:40:43
  * @LastEditors: canlong.shen
- * @LastEditTime: 2023-05-10 13:55:36
- * @FilePath: \common\src\components\bsgoal-base-link\demo.vue
+ * @LastEditTime: 2023-07-15 19:36:57
+ * @FilePath: \v3_basic_component\src\components\bsgoal-base-link\demo.vue
  * @Description: 链接公共组件 演示
  * 
 -->
@@ -24,7 +24,7 @@ const clickLink = () => {
 </script>
 <template>
   <div class="bsgoal-base-link-demo">
-    <BsgoalBaseLink content="这是链接" @on-click="clickLink" />
+    <BsgoalBaseLink type="danger"  content="这是链接" @on-click="clickLink" />
     <BsgoalBaseLink underline content="这是链接" @on-click="clickLink" />
   </div>
 </template>
--- src/components/bsgoal-base-link/index.vue
+++ src/components/bsgoal-base-link/index.vue
@@ -2,8 +2,8 @@
  * @Author: canlong.shen
  * @Date: 2023-05-10 13:40:36
  * @LastEditors: canlong.shen
- * @LastEditTime: 2023-05-10 13:55:15
- * @FilePath: \common\src\components\bsgoal-base-link\index.vue
+ * @LastEditTime: 2023-07-15 19:35:49
+ * @FilePath: \v3_basic_component\src\components\bsgoal-base-link\index.vue
  * @Description: 链接 公共组件
  * 
 -->
@@ -11,34 +11,41 @@
 <script setup>
 /* setup模板
 ---------------------------------------------------------------- */
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 defineOptions({
   name: 'BsgoalBaseLink'
 })
 
 const props = defineProps({
-    /**
-     * 内容
-     */
-    content: {
-        type: [String],
-        default: 'link'
-    },
-    /**
-     * 是否下划线
-     */
-    underline: {
-        type: [Boolean],
-        default: false
-    },
-    /**
-     * style 对象
-     */
-    styler: {
-        type: [Object],
-        default: () => ({})
-    }
+  /**
+   * 内容
+   */
+  content: {
+    type: [String],
+    default: 'link'
+  },
+  /**
+   * 是否下划线
+   */
+  underline: {
+    type: [Boolean],
+    default: false
+  },
+  /**
+   * style 对象
+   */
+  styler: {
+    type: [Object],
+    default: () => ({})
+  },
+  /**
+   *  类型
+   */
+  type: {
+    type: [String],
+    default: 'primary'
+  }
 })
 
 const emits = defineEmits(['on-click'])
@@ -46,22 +53,25 @@ const emits = defineEmits(['on-click'])
 /**
  * @Author: canlong.shen
  * @description: 点击链接
- * @default: 
+ * @default:
  * @return {*}
  */
 const clickLink = () => {
-    emits('on-click')
+  emits('on-click')
 }
-
-
-
 </script>
 <template>
-    <div class="bsgoal-base-link">
-        <el-link class="bsgoal_base_link" type="primary" :underline="underline" :style="styler" @click="clickLink">
-            <slot>{{ content }}</slot>
-        </el-link>
-    </div>
+  <div class="bsgoal-base-link">
+    <el-link
+      class="bsgoal_base_link"
+      :type="type"
+      :underline="underline"
+      :style="styler"
+      @click="clickLink"
+    >
+      <slot>{{ content }}</slot>
+    </el-link>
+  </div>
 </template>
 <style lang="scss" scoped>
 /* 自定义样式
